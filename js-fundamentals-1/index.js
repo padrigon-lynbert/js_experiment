4f33f1da97a678bfc549b3946c83ea7c5863dec9
siv <padrigonlynbert@gmail.com>
2026-02-25 15:45:33 +0800

    talking to backend

--- js-fundamentals-1/index.js
+++ js-fundamentals-1/index.js
@@ -11,3 +11,11 @@ btn.addEventListener('click', function() {
 showBtn.addEventListener('click', function() {
     title.textContent = input.value;
 });
+
+const load = document.querySelector('#load');
+
+load.addEventListener('click', async function () {
+    const res = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+    const data = await res.json();
+    title.textContent = data.title;
+});
